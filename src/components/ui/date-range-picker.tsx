849da2040a76ef03b1ece2c255c4f8dcdd930cce
Moneer <xv.neer.business@gmail.com>
2025-07-04 15:45:32 +0300

    fix error

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -2,7 +2,11 @@
 
 import React from "react"
 import { cn } from "@/utils/cn"
-import { getLocalTimeZone, today, type CalendarDate } from "@internationalized/date"
+import {
+  getLocalTimeZone,
+  today,
+  type CalendarDate,
+} from "@internationalized/date"
 import { RangeCalendar } from "@nextui-org/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover"
 import { CalendarDaysIcon } from "lucide-react"
@@ -21,7 +25,7 @@ type Props = {
         start: CalendarDate
         end: CalendarDate
       }>
-    >,
+    >
   ]
 }
 
@@ -38,8 +42,9 @@ const DateRangePicker = ({ state, className, classNames }: Props) => {
                 className={cn(
                   " block h-16 w-full rounded-large  bg-default-100 px-3",
                   " py-2.5 duration-150 hover:bg-default-200 md:h-20 lg:h-20 ",
-                  classNames?.button,
-                )}>
+                  classNames?.button
+                )}
+              >
                 <span className="   flex items-center justify-center  gap-2 text-sm text-default-600">
                   <CalendarDaysIcon size={18} />
                   تاريخ الدخول
@@ -53,8 +58,9 @@ const DateRangePicker = ({ state, className, classNames }: Props) => {
                 className={cn(
                   " block h-16 w-full rounded-large  bg-default-100 px-3",
                   "py-2.5 duration-150 hover:bg-default-200 md:h-20 lg:h-20 ",
-                  classNames?.button,
-                )}>
+                  classNames?.button
+                )}
+              >
                 <span className="   flex items-center justify-center  gap-2 text-sm text-default-600">
                   <CalendarDaysIcon size={18} />
                   تاريخ الخروج
@@ -67,6 +73,7 @@ const DateRangePicker = ({ state, className, classNames }: Props) => {
         <PopoverContent className="!bg-transparent !p-0 !shadow-none">
           <div dir="ltr" lang="en" className=" hidden md:block">
             <RangeCalendar
+              // @ts-ignore
               value={value}
               onChange={setValue}
               pageBehavior="single"
@@ -77,6 +84,7 @@ const DateRangePicker = ({ state, className, classNames }: Props) => {
           </div>
           <div dir="ltr" lang="en" className="block md:hidden">
             <RangeCalendar
+              // @ts-ignore
               value={value}
               onChange={setValue}
               pageBehavior="single"
